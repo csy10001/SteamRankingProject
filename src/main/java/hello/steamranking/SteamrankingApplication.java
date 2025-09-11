9e37014b43c244ed7bf55d8cfa06d30828e3b49e
csy10001 <fuitshine@gmail.com>
2025-09-11 17:31:40 +0900

    first commit

---
+++ src/main/java/hello/steamranking/SteamrankingApplication.java
@@ -0,0 +1,13 @@
+package hello.steamranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamrankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamrankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/steamranking/SteamrankingApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.steamranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamrankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
